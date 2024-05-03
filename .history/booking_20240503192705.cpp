1adc4fc79992b71bac664416ee3c94f53a882ae6
Haakim19 <haakimajmal7@ggmail.com>
2024-05-03 22:45:00 +0530

    final project

---
+++ .history/booking_20240503192705.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int signIn_logIn();
+int Destination();
+int Passport_number();
+int amount();
+int currency();
+int bill();
+
+void greeting_start()
+{
+    cout << "\t" <<"      ----- WELLCOME -----      " << endl ;
+    cout << "\t" <<"**** THOUSAND SUNNY AIRLINE ****" << endl ;
+    
+
+}
+
+int main()
+{
+    greeting_start();
+}
---
+++ .history/booking_20240503194908.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int signIn_logIn();
+int Destination();
+int Passport_number();
+int amount();
+int currency();
+int bill();
+
+void greeting_start()
+{
+    cout << "\t" <<"      ----- WELLCOME -----      " << endl ;
+    cout << "\t" <<"**** THOUSAND SUNNY AIRLINE ****" << endl ;
+}
+
+int signIn_logIn()
+{
+    string signIN,logIN;
+    cout << "SIGN IN OR LOGIN";
+    getline (cin,  )
+}
+
+
+int main()
+{
+    greeting_start();
+}
